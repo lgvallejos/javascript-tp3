d85c9cb20720a0a94ccf43641b7b396a4ce8cc95
lgvallejos <gabrielvallejos86@gmail.com>
2016-12-03 13:54:56 -0300

    trabajo practico 3

---
+++ script.js
@@ -0,0 +1,23 @@
+function suma(a,b){
+
+	console.log('La suma es: ' + (parseInt(a) + parseInt(b)));
+
+};
+
+function resto(a,b) {
+
+	console.log('La resta es: ' + (parseInt(a) - parseInt(b)));
+
+};
+
+function multiplico(a,b) {
+
+	console.log('La multiplicación es: ' + (parseInt(a) * parseInt(b)));
+
+};
+
+function divido(a,b) {
+
+	console.log('La división es: ' + (parseInt(a) / parseInt(b)));
+
+};
